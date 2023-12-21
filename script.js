26eb7717bc504e90881f3cca9cd51066b1066255
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-21 08:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = name,
+		this.age = age
+	}
+	get (){
+		return this.name;
+	}
+	set (aged){
+		this.aged = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	tech(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
